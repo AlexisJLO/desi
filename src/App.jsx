2cc86a8b02e90cdfad5775ae933ebc686af92e95
Alexis Lopez <121430851+AlexisJLO@users.noreply.github.com>
2026-07-08 10:02:03 -0500

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import './App.css'
 import './index.css'
 
 function App() {
+   const [clicked, setClicked] = useState(false);
   return (
     <div className="hero">
       <img src={helloKitty}
@@ -14,6 +15,13 @@ function App() {
         className="hello-kitty"/>
       <h1 className="title">Happy National Girlfriend Day!</h1>
       <p className="subtitle">To the most beautiful girlfriend in the world!</p>
+    <button
+      className={`button ${clicked ? "" : "pop-animation"}`}
+      onClick={() => setClicked(true)}
+    >
+      I Love You 💖
+    </button>
+  
     </div>
   );
 }
