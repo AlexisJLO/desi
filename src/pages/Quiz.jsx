8b3ecc149df87206fa7c0afc6220efa25ab984a1
Alexis Lopez <121430851+AlexisJLO@users.noreply.github.com>
2026-07-10 12:22:48 -0500

    added quiz page

---
+++ src/pages/Quiz.jsx
@@ -0,0 +1,12 @@
+function Quiz() {
+  return (
+    <div>
+      <h1>Quiz Time! 💖</h1>
+      
+      <button className="button" onClick={() => window.location.href = "/"}>Go Back</button>
+    </div>
+
+  );
+}
+
+export default Quiz;
