817c2844aad7113dc7606005c3016c21549dceb4
Alexis Lopez <121430851+AlexisJLO@users.noreply.github.com>
2026-08-04 11:28:27 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App.jsx";
 import Quiz from "./pages/Quiz.jsx";
@@ -8,12 +8,12 @@ import Memories from "./pages/Memories.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/desi" element={<App />} />
+        <Route path="/" element={<App />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/memories" element={<Memories />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
