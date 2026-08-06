43b7ffe53498bdee4cbb2e0ff1c44dada391e869
Alexis Lopez <121430851+AlexisJLO@users.noreply.github.com>
2026-08-06 15:06:25 -0500

    a

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,7 @@ import babel from '@rolldown/plugin-babel'
 export default defineConfig({
   plugins: [
     react(),
-    
     babel({ presets: [reactCompilerPreset()] })
   ],
-  server: {
-    port: 3000,
-    open: true,
-  },
+  
 })
